d1813b8ff4661ab26d4de902c0e65f70c7402ab8
Ezequiel Marin <ezequiel.marin@mi.unc.edu.ar>
2024-04-19 23:04:31 -0300

    Correciones funciones Interp, compila correctamente

--- src/Interp.hs
+++ src/Interp.hs
@@ -1,11 +1,7 @@
-module Interp
-  ( interp,
-    initial,
-  )
-where
+module Interp where
 
-import Dibujo
-import FloatingPic
+import Dibujo(Dibujo, foldDib)
+import FloatingPic (FloatingPic, Output, Conf(Conf), grid, half, zero)
 import Graphics.Gloss (Display (InWindow), color, display, makeColorI, pictures, translate, white, Picture)
 import qualified Graphics.Gloss.Data.Point.Arithmetic as V
 
@@ -13,6 +9,9 @@ import qualified Graphics.Gloss.Data.Point.Arithmetic as V
 -- pantalla la figura de la misma de acuerdo a la interpretación para
 -- las figuras básicas. Permitimos una computación para poder leer
 -- archivos, tomar argumentos, etc.
+
+
+
 initial :: Conf -> Float -> IO ()
 initial (Conf n dib intBas) size = display win white $ withGrid fig size
   where
@@ -22,12 +21,12 @@ initial (Conf n dib intBas) size = display win white $ withGrid fig size
     withGrid p x = translate desp desp $ pictures [p, color grey $ grid (ceiling $ size / 10) (0, 0) x 10]
     grey = makeColorI 100 100 100 100
 
---- Figuras
+{- --- Figuras
 rectangle :: FloatingPic
-rectangle x y w = line [x, x V.+ y, x V.+ y V.+ w, x V.+ w, x]
+rectangle x y w = lines [x, x V.+ y, x V.+ y V.+ w, x V.+ w, x]
 
 triangle :: FloatingPic
-triangle x y w = line $ map (x V.+) [(0,0), y V.+ half w, w, (0,0)]
+triangle x y w = lines $ map (x V.+) [(0,0), y V.+ half w, w, (0,0)]
 
 crux :: FloatingPic
 crux x y w = pictures [line [x, x V.+ y V.+ w], line [x V.+ y, x V.+ w]]
@@ -48,26 +47,25 @@ efe x y w = line . map (x V.+) $ [
 
 --- V.+ y V.- serian la suma y resta de vectores
 --- V.* multiplicacion
---- V.negate es negar el vector
+--- V.negate es negar el vector -}
 
-half :: Vector -> Vector
-half v = (0.5 V.* v)
 
 -- Interpretación de (^^^)
+-- Es como una base para juntar dos figuras, con esta hacemos sup
 ov :: Picture -> Picture -> Picture
-ov = undefined
+ov picp picq = pictures [picp, picq]
 
 r45 :: FloatingPic -> FloatingPic
 r45 f d w h = f (d V.+ half(w V.+ h))   (half(w V.+ h)) (half(h V.- w))
 
 rot :: FloatingPic -> FloatingPic
-rot f d w h = f  (d V.+ w) (h) (V.negate w)
+rot f d w h = f  (d V.+ w) h (zero V.- w) -- o tmb V.negate w
 
 esp :: FloatingPic -> FloatingPic
-esp f d w h = f (d V.+ w) (V.negate w) (h)
+esp f d w h = f (d V.+ w) (zero V.- w) h
 
 sup :: FloatingPic -> FloatingPic -> FloatingPic
-sup f1 f2 d w h = pictures [f1 d w h, f2 d w h]
+sup f1 f2 d w h = ov (f1 d w h) (f2 d w h)
 
 jun :: Float -> Float -> FloatingPic -> FloatingPic -> FloatingPic
 jun m n f1 f2 d w h = pictures [f1 (d) (w') (h), f2 (d V.+ w') (r' V.* w) (h)]
@@ -75,15 +73,14 @@ jun m n f1 f2 d w h = pictures [f1 (d) (w') (h), f2 (d V.+ w') (r' V.* w) (h)]
     r' = n / (m + n)
     r = m / (m + n)
     w' = r V.* w
+-- Api es una funcion que recibe dos figuras y las pone una arriba de la otra, no superpuestas
 
 api :: Float -> Float -> FloatingPic -> FloatingPic -> FloatingPic
-api x y f1 f2 d w h = pictures [f1 (d V.+ h') (w) (r V.* h), f2 () () ()]
+api m n f g d w h = ov (f (d V.+ h' ) w (r V.* h)) (g d w h')
   where
     r' = n / (m + n)
     r = m / (m + n)
     h' = r' V.* h
 
-interpRotar = rot (interp f a)
-
 interp :: Output a -> Output (Dibujo a)
-interp f a = foldDib f rot es r45 api jun ov a
+interp f = foldDib f rot esp r45 api jun sup 
